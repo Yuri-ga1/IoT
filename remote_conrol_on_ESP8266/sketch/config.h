952087fe6f03ee1d12efe788fd2e81765084f2f4
Yuri-ga1 <regota09@gmail.com>
2023-10-17 11:11:20 +0800

    Update config.h

--- remote_conrol_on_ESP8266/sketch/config.h
+++ remote_conrol_on_ESP8266/sketch/config.h
@@ -2,8 +2,8 @@ String AP_NAME = "Pivo"; // AP WIFI Access Point
 String AP_PASSWORD = "PivoDryg"; // AP WIFI
 
 //CLI Wifi client
-const char* CLI_SSID = "Pivo"; // turn phone 5 GHz -> 2.4 GHz
-const char* CLI_PASS = "jodo6303";
+const char* CLI_SSID = "Your wifi name"; // turn phone 5 GHz -> 2.4 GHz
+const char* CLI_PASS = "Your wifi pass";
 
 int WEB_SERVER_PORT = 80;
 
