cd0bcdb4b8cc704d623e17f5c8441ab72ec39069
Yuri-ga1 <regota09@gmail.com>
2023-10-24 09:49:43 +0800

    input topic

--- ESP_Lamp/work4/test_client.py
+++ ESP_Lamp/work4/test_client.py
@@ -5,24 +5,23 @@ import paho.mqtt.client as paho
 def ligth(a: int, b: int):
     client.publish(topic, 1)
     print("off")
-    time.sleep(20+a)
+    time.sleep(5+a)
     client.publish(topic, 0)
     print("on")
-    time.sleep(20-b-a)
+    time.sleep(5-b-a)
     client.publish(topic, 1)
     print("off")
-    time.sleep(20+b)
+    time.sleep(5+b)
 
 broker = "broker.emqx.io"
 client = paho.Client("your_name")
 client.connect(broker)
 client.loop_start()
-topic = input('Give me a topic: ')
-
+topic = "tyt1/command"
 
 timings = np.array((0, 0))
 while True:
-    if timings.sum() == 11:
+    if timings.sum() == 5:
         timings = np.array((0, 0))
 
     if timings.sum()%2==0:
@@ -31,3 +30,7 @@ while True:
     else:
         ligth(timings[0], timings[1])
         timings[0]+=1
+
+
+client.disconnect()
+client.loop_stop()
